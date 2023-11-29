b885e0b16287ce4d2897dfb994ae47d2f26650e9
Maximilian Maroe <drakfyre@gmail.com>
2023-11-28 21:21:24 -0800

    ↔→IIL⌠≥≤LOI↓≤

--- BrainfuckInterpreter/BrainfuckInterpreter.cpp
+++ BrainfuckInterpreter/BrainfuckInterpreter.cpp
@@ -6,6 +6,8 @@
 
 using std::string;
 
+void ScanForBracket(char*& currentCharacter, char bracketType);
+
 int main()
 {
     std::cout << "Please type in your Brainfuck program now.\n";
@@ -13,52 +15,127 @@ int main()
     string brainfuckString;
     std::cin >> brainfuckString;
 
-    uint8_t data[30000];
+    uint8_t data[30000] = { 0 };
     uint8_t* dataPointer = data;
 
     char* currentCharacter = &brainfuckString[0];
 
 
-    while (currentCharacter != '\0')
+    while (*currentCharacter != '\0')
     {
         switch (*currentCharacter)
         {
             case '>':
                 // Increment the data pointer by one (to point to the next cell to the right).
                 dataPointer++;
+                currentCharacter++;
                 break;
             case '<':
                 // Decrement the data pointer by one (to point to the next cell to the left).
                 dataPointer--;
+                currentCharacter++;
                 break;
             case '+':
                 // Increment the byte at the data pointer by one.
-                *dataPointer++;
+                (*dataPointer)++;
+                currentCharacter++;
                 break;
             case '-':
                 // Decrement the byte at the data pointer by one.
-                *dataPointer--;
+                (*dataPointer)--;
+                currentCharacter++;
                 break;
             case '.':
                 // 	Output the byte at the data pointer.
                 std::cout << *dataPointer;
+                currentCharacter++;
                 break;
             case ',':
                 // Accept one byte of input, storing its value in the byte at the data pointer.
-                std::cin >> *dataPointer;
+                std::cin >> dataPointer;
+                currentCharacter++;
                 break;
             case '[':
                 // 	If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
                 if (*dataPointer == 0)
                 {
                     // Scan forward and set data pointer to matching ]
+                    ScanForBracket(currentCharacter, '[');
+                }
+                else
+                {
+                    currentCharacter++;
                 }
                 break;
             case ']':
                 // If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
+                if (*dataPointer != 0)
+                {
+                    // Scan backward and set data pointer to matching [
+                    ScanForBracket(currentCharacter, ']');
+                }
+                else
+                {
+                    currentCharacter++;
+                }
                 break;
             default:
                 break;
+        }
+    }
+}
+
+void ScanForBracket(char*& currentCharacter, char bracketType)
+{
+    int bracketCount = 0;
+    char otherBracket;
+    bool scanBackwards = false;
+
+    if(bracketType == '[')
+    {
+        otherBracket = ']';
+    }
+    else
+    {
+        otherBracket = '[';
+        scanBackwards = true;
+    }
+
+
+    while(true)
+    {
+        if(scanBackwards)
+        {
+            currentCharacter--;
+        }
+        else
+        {
+            currentCharacter++;
+        }
+
+        if (*currentCharacter == bracketType)
+        {
+            bracketCount++;
+        }
+        else if (*currentCharacter == otherBracket)
+        {
+            if (bracketCount > 0)
+            {
+                bracketCount--;
+            }
+            else
+            {
+                // We are already at the right bracket, but we want to be the command before/after it
+                if (scanBackwards)
+                {
+                    currentCharacter--;
+                }
+                else
+                {
+                    currentCharacter++;
+                }
+                return;
+            }
         }
     }
 }
