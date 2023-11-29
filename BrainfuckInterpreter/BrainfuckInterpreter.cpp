3c88676c8c438e5cec1384ed40a12a4faa6b65bd
Maximilian Maroe <drakfyre@gmail.com>
2023-11-28 18:19:59 -0800

    Some WIP implementation

--- BrainfuckInterpreter/BrainfuckInterpreter.cpp
+++ BrainfuckInterpreter/BrainfuckInterpreter.cpp
@@ -13,31 +13,46 @@ int main()
     string brainfuckString;
     std::cin >> brainfuckString;
 
+    uint8_t data[30000];
+    uint8_t* dataPointer = data;
+
     char* currentCharacter = &brainfuckString[0];
+
+
     while (currentCharacter != '\0')
     {
         switch (*currentCharacter)
         {
             case '>':
                 // Increment the data pointer by one (to point to the next cell to the right).
+                dataPointer++;
                 break;
             case '<':
                 // Decrement the data pointer by one (to point to the next cell to the left).
+                dataPointer--;
                 break;
             case '+':
                 // Increment the byte at the data pointer by one.
+                *dataPointer++;
                 break;
             case '-':
                 // Decrement the byte at the data pointer by one.
+                *dataPointer--;
                 break;
             case '.':
                 // 	Output the byte at the data pointer.
+                std::cout << *dataPointer;
                 break;
             case ',':
                 // Accept one byte of input, storing its value in the byte at the data pointer.
+                std::cin >> *dataPointer;
                 break;
             case '[':
                 // 	If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+                if (*dataPointer == 0)
+                {
+                    // Scan forward and set data pointer to matching ]
+                }
                 break;
             case ']':
                 // If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
