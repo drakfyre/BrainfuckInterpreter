4fb0d0ff12992d15a181d07f652b13d09dec5273
Maximilian Maroe <drakfyre@gmail.com>
2023-12-21 22:14:25 -0800

    Some work on copy command, it's gonna be a little trickier than I thought

--- BrainfuckInterpreter/BrainfuckTool.cpp
+++ BrainfuckInterpreter/BrainfuckTool.cpp
@@ -1,12 +1,13 @@
 #include "BrainfuckTool.h"
 #include <iostream>
 
+// This currently assumes that the value we are setting is 0
 void BrainfuckTool::SetValue(int value)
 {
 	for (int i = 0; i < value; i++)
 	{
 		//std::cout << '+';
-		bfString += '+';
+		BrainfuckTool::Add();
 	}
 }
 
@@ -43,6 +44,16 @@ void BrainfuckTool::Left(int amount)
 	}
 }
 
+void BrainfuckTool::Add()
+{
+	bfString += '+';
+}
+
+void BrainfuckTool::Minus()
+{
+	bfString += '-';
+}
+
 void BrainfuckTool::Out(int length)
 {
 	for (int i = 0; i < length; i++)
@@ -52,6 +63,16 @@ void BrainfuckTool::Out(int length)
 	}
 }
 
+void BrainfuckTool::Branch()
+{
+	bfString += '[';
+}
+
+void BrainfuckTool::Loop()
+{
+	bfString += ']';
+}
+
 // Copies value from current memory position to offset from current memory positon
 void BrainfuckTool::CopyTo(int offset)
 {
@@ -59,5 +80,15 @@ void BrainfuckTool::CopyTo(int offset)
 	// We kinda can't make this non-destructive in some way, at least in interim
 	// We may need to have an offset to a scratch pad area, but let's try to avoid that
 	// We will currently assume that the destination contains 0, but we'll circle back around on that later
-	
+	// We are done copying if the current memory position value is 0 (at which point we'll restore it somehow)
+	Branch(); // If current value is 0 we end up at associated loop
+	Right(offset);
+	Add();
+	Left(offset);
+	Minus();
+	Loop();
+
+	// At this point I *think* we've now put a 0 in our original location, and our original value is now at offset
+	// Yeah... there's no way to avoid a scratch pad for a copy, I don't think
+	// Also looking more and more that it would be helpful to have some kind of assembler/memory pointer tracking system
 }
--- BrainfuckInterpreter/BrainfuckTool.h
+++ BrainfuckInterpreter/BrainfuckTool.h
@@ -8,7 +8,11 @@ public:
 	void SetArray(char inputArray[], int arrayLength, int arrayWidth = 0);
 	void Right(int amount = 1);
 	void Left(int amount = 1);
+	void Add();
+	void Minus();
 	void Out(int length = 1);
+	void Branch();
+	void Loop();
 	void CopyTo(int offset);
 
 	std::string bfString = "";
