2327266779d09692d214f8d831e4e745592978f8
Maximilian Maroe <drakfyre@gmail.com>
2023-12-21 21:45:23 -0800

    Planning it out

--- BrainfuckInterpreter/BrainfuckInterpreter.cpp
+++ BrainfuckInterpreter/BrainfuckInterpreter.cpp
@@ -33,8 +33,22 @@ int main()
     // Put level array into memory (at position 0) with newlines built-in based on width (I think this will be easier to work with in the long-run)
     tool.SetArray(levelArray, 25*15, 25);
 
+    // Set our starting player position into memory (at position 25*15+15)
+    // Using 3 and 5 because they are prime numbers greater than 2 so we should know when we're off by a bit
+    tool.SetValue(3);
+    tool.Right();       // 25*15 + 15 + 1
+    tool.SetValue(5);
+    tool.Right();       // 25*15 + 15 + 2
+
+    // Okay, so in our original progam, during our output function if the position matches the player, we output @
+    // Doing that during the output in Brainfuck would require us to thrash around between memory, checking every spot to see if it should be @
+    // Remember, distance in brainfuck is more >>> and <<< so we want to minimize jumps (this is also why I encoded \n into the array)
+    // So in our new program, what we will do is actually PUT the @ symbol in the position inside the map itself
+    // So that the output code can just output stuff and not jump around
+    // So I guess that code will go here
+
     // Move back to position 0 (move left size of the array, it's 25*15 + 15 characters because of the newlines we stuffed in the SetArray function)
-    tool.Left(25*15 + 15);
+    tool.Left(25*15 + 15 + 2);
 
     // Finally, we output the whole thing
     tool.Out(25*15 + 15);
