e9ea6a9edfac7f52209bcaf7246e42e70de453cd
Maximilian Maroe <drakfyre@gmail.com>
2024-01-06 18:14:22 -0800

    Well, it's working, at least for moving left and right!

--- BrainfuckInterpreter/Main.cpp
+++ BrainfuckInterpreter/Main.cpp
@@ -49,16 +49,16 @@ int main()
     int gameMapIndex = tool.NewArray(levelArray, kLevelWidth * kLevelHeight, kLevelWidth);
     int oneAfterGameMapIndex = tool.NewVariable(0);
 
-    tool.ChangeIndexRelative(gameRunning - oneAfterGameMapIndex - 2);
-    tool.Branch(true); // False forces immediate resolve of this branch (Can only be done with branches that will not be skipped 1st time or we'll crash)
-        tool.ChangeIndexRelative(gameMapIndex - gameRunning);
-        tool.OutString(kLevelWidth * kLevelHeight + kLevelHeight); // Level width*height + 1 per height (because of the newlines we added in NewArray)
-        tool.ChangeIndexRelative(inputCharacterIndex - (oneAfterGameMapIndex));
-        tool.In();
-        tool.PlayerLogic(inputCharacterIndex, wIndex, aIndex, sIndex, dIndex, wIndexTemp, aIndexTemp, sIndexTemp, dIndexTemp, playerPositionIndex, playerPositionIndexTemp, gameMapIndex, widthIndex);
-        tool.ChangeIndexRelative(gameRunning - inputCharacterIndex);
-        tool.Plus();    // Force loop while I figure this thing out
-    tool.Loop();
+    //tool.ChangeIndexRelative(gameRunning - oneAfterGameMapIndex - 2);
+    //tool.Branch(true); // False forces immediate resolve of this branch (Can only be done with branches that will not be skipped 1st time or we'll crash)
+    //    tool.ChangeIndexRelative(gameMapIndex - gameRunning);
+    //    tool.OutString(kLevelWidth * kLevelHeight + kLevelHeight); // Level width*height + 1 per height (because of the newlines we added in NewArray)
+    //    tool.ChangeIndexRelative(inputCharacterIndex - (oneAfterGameMapIndex));
+    //    tool.In();
+    //    tool.PlayerLogic(inputCharacterIndex, wIndex, aIndex, sIndex, dIndex, wIndexTemp, aIndexTemp, sIndexTemp, dIndexTemp, playerPositionIndex, playerPositionIndexTemp, gameMapIndex, widthIndex);
+    //    tool.ChangeIndexRelative(gameRunning - inputCharacterIndex);
+    //    tool.Plus();    // Force loop while I figure this thing out
+    //tool.Loop();
 
     // Okay, so in our original progam, during our output function if the position matches the player, we output @
     // Doing that during the output in Brainfuck would require us to thrash around between memory, checking every spot to see if it should be @
@@ -78,8 +78,8 @@ int main()
 
     string brainfuckString = buffer.str();
 
-    //BFVM vm(brainfuckString);
-    //while (vm.Step(false));
+    BFVM vm(brainfuckString);
+    while (vm.Step(false));
 
 
     //std::cout << "Please type in your Brainfuck program now.\n";
