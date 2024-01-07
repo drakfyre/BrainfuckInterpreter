9a9ba8ead2505c1dbf9ba0ce19f6badab1788c97
Maximilian Maroe <drakfyre@gmail.com>
2024-01-06 19:40:08 -0800

    Reogranizing BrainfuckTool header and cpp

--- BrainfuckInterpreter/BrainfuckTool.cpp
+++ BrainfuckInterpreter/BrainfuckTool.cpp
@@ -1,64 +1,7 @@
 #include "BrainfuckTool.h"
 #include <iostream>
 
-// This currently assumes that the value we are setting is 0
-void BrainfuckTool::SetValue(int value)
-{
-	AddValue(value);
-}
-
-void BrainfuckTool::AddValue(int value)
-{
-	for (int i = 0; i < value; i++)
-	{
-		BrainfuckTool::Plus();
-	}
-}
-
-
-void BrainfuckTool::SubtractValue(int value)
-{
-	for (int i = 0; i < value; i++)
-	{
-		BrainfuckTool::Minus();
-	}
-}
-
-void BrainfuckTool::SetArray(char inputArray[], int arrayLength, int arrayWidth)
-{
-	for (int i = 0; i < arrayLength; i++)
-	{
-		SetValue(inputArray[i]);
-		Right();
-		Right();
-
-		if (i % arrayWidth == arrayWidth - 1)
-		{
-			SetValue('\n');
-			Right();
-			Right();
-		}
-	}
-}
-
-// One of the "memory aware" functions, this will set the current variable to a value and move over 2 in prep for more variable declarations
-int BrainfuckTool::NewVariable(int value)
-{
-	int returnVal = virtualDataIndex;
-	SetValue(value);
-	Right();
-	Right();
-	return returnVal;
-}
-
-
-// We're only going to use this for our map, so it doesn't prep for more variables; gonna put the map at the end
-int BrainfuckTool::NewArray(char inputArray[], int arrayLength, int arrayWidth)
-{
-	int returnVal = virtualDataIndex;
-	SetArray(inputArray, arrayLength, arrayWidth);
-	return returnVal;
-}
+/// Built-in brainfuck
 
 void BrainfuckTool::Right()
 {
@@ -72,7 +15,7 @@ void BrainfuckTool::Right()
 		executionQueue++;
 	}
 	virtualDataIndex++;
-	if(virtualDataIndex >= 30000)
+	if (virtualDataIndex >= 30000)
 	{
 		virtualDataIndex = 0;
 	}
@@ -94,60 +37,6 @@ void BrainfuckTool::Left()
 	{
 		virtualDataIndex = 30000 - 1;
 	}
-}
-
-void BrainfuckTool::ChangeIndexRelative(int offset)
-{
-	while(offset > 0)
-	{
-		Right();
-		offset--;
-	}
-	while(offset < 0)
-	{
-		Left();
-		offset++;
-	}
-}
-
-// Untested, may not be totally useful
-void BrainfuckTool::ChangeIndexToNextZero()
-{
-	Branch();	// Ends if we're currently at a 0
-		Right();	// Increase current index
-	Loop();		// If current index is 0, ends
-}
-
-void BrainfuckTool::ChangeIndexToNextTempZero()
-{
-	Branch();	// Ends if we're currently at a 0
-		Right();	// Increase current index
-		Right();	// Increase current index
-	Loop();		// If current index is 0, ends
-}
-
-void BrainfuckTool::ChangeIndexToPreviousTempZero()
-{
-	Branch();	// Ends if we're currently at a 0
-		Left();	// Decrease current index
-		Left();	// Decrease current index
-	Loop();		// If current index is 0, ends
-}
-
-// This isn't working because Not is destructive and we're already using the temp variables
-// So gonna bring back NotInMyBackyard (and make it actually work?)
-// Or just gonna gut Not out of this
-void BrainfuckTool::ChangeIndexToPreviousTempOne()
-{
-	Minus();
-	Branch();	// Ends if we're currently at a 1
-		Plus();	// Reverse what we did before our loop
-		Left();	// Decrease current index
-		Left();	// Decrease current index
-		Minus();
-	Loop();		// If current index is 1, ends
-
-	// Do we want to clear our 1 here?
 }
 
 void BrainfuckTool::Plus()
@@ -202,22 +91,11 @@ void BrainfuckTool::Out()
 	}
 }
 
-void BrainfuckTool::OutString(int length)
-{
-	while (length > 0)
-	{
-		Out();
-		Right();
-		Right();
-		length--;
-	}
-}
-
 void BrainfuckTool::Branch(bool waitToResolve)
 {
 	bfvm.brainfuckString += '[';
 
-	if(waitToResolve)
+	if (waitToResolve)
 	{
 		executionQueue++;
 		bracketDepth++;
@@ -233,7 +111,7 @@ void BrainfuckTool::Loop(bool resolveLoops)
 	bfvm.brainfuckString += ']';
 	executionQueue++;
 	bracketDepth--;
-	if(bracketDepth < 0)
+	if (bracketDepth < 0)
 	{
 		bracketDepth = 0;
 	}
@@ -249,84 +127,46 @@ void BrainfuckTool::Loop(bool resolveLoops)
 	}
 }
 
-// Moves value from current memory position to offset from current memory position, assuming that the destination contains 0
-// Does this mean that this could be generally considered "AddTo"? for the cases that are non-zero? (But that doesn't imply the move...)
-// Not sure how useful this actually is, just happened to write it by accident when trying to make a more useful function
-// (Lightly tested)
-void BrainfuckTool::MoveToOffset(int offset)
-{
-	Branch();						// If current value is 0 we end up at associated loop; we'd be done in that case anyway so that's perfect
-		ChangeIndexRelative(offset);	// We move to our destination
-		Plus();							// We add 1 to our destination
-		ChangeIndexRelative(-offset);	// We move back to our source
-		Minus();						// We subtract 1 from our source
-	Loop();							// If our source is 0, stop looping
-}
 
-void BrainfuckTool::NewTempVariable()
-{
-	Right();
-}
-
-// Copies value from current memory position to offset from current memory positon, assuming that the destination contains 0
-// (Lightly tested)
-void BrainfuckTool::CopyToOffset(int offset)
-{
-	int origininalOffset = 0;
-	int counter = 1;
-	
-	NewTempVariable();
-	ChangeIndexRelative(origininalOffset - counter);
-	MoveToOffset(counter - origininalOffset);
-
-	ChangeIndexRelative(counter - origininalOffset);
-
-	Branch();						// If current value is 0 we end up at associated loop; we'd be done in that case anyway so that's perfect
-		ChangeIndexRelative(origininalOffset - counter);	// We change index to our origin (which now contains 0)
-		Plus();									// We add 1 to our origin
-		ChangeIndexRelative(offset);			// We change index to our destination
-		Plus();									// We add 1 our destination
-		ChangeIndexRelative(-offset);			// We go back to origin
-		ChangeIndexRelative(counter - origininalOffset);	// We change index to our counter
-		Minus();								// Subtract 1 from our counter
-	Loop();							// Loop till counter is 0
-	ChangeIndexRelative(origininalOffset - counter);
-}
-
-// I suspect there's a bug here, we hang on w currently
-void BrainfuckTool::SubtractFromOffset(int offset)
-{
-	int originalOffset = 0;
-	int counter = 1;
-
-	NewTempVariable();
-	ChangeIndexRelative(originalOffset - counter);
-	MoveToOffset(counter);
-
-	ChangeIndexRelative(counter - originalOffset);
-
-	Branch();						// If current value is 0 we end up at associated loop; we'd be done in that case anyway so that's perfect
-		ChangeIndexRelative(originalOffset - counter);	// We change index to our origin (which now contains 0)
-		Plus();							// We add 1 to our origin
-		ChangeIndexRelative(offset);	// We change index to our destination
-		Minus();						// We subtract 1 from our destination
-		ChangeIndexRelative(-offset);			// We go back to origin
-		ChangeIndexRelative(counter - originalOffset);	// We change index to our counter
-		Minus();						// Subtract 1 from our counter
-		Loop();							// Loop till counter is 0
-	ChangeIndexRelative(originalOffset - counter);
-}
-
-void BrainfuckTool::AddToOffset(int offset)
-{
-	CopyToOffset(offset);
-}
+/// Pure brainfuck functions
 
 void BrainfuckTool::SetZero()
 {
 	Branch();
 		Minus();
 	Loop();
+}
+
+void BrainfuckTool::ChangeIndexToNextTempZero()
+{
+	Branch();	// Ends if we're currently at a 0
+		Right();	// Increase current index
+		Right();	// Increase current index
+	Loop();		// If current index is 0, ends
+}
+
+void BrainfuckTool::ChangeIndexToPreviousTempZero()
+{
+	Branch();	// Ends if we're currently at a 0
+		Left();	// Decrease current index
+		Left();	// Decrease current index
+	Loop();		// If current index is 0, ends
+}
+
+// This isn't working because Not is destructive and we're already using the temp variables
+// So gonna bring back NotInMyBackyard (and make it actually work?)
+// Or just gonna gut Not out of this
+void BrainfuckTool::ChangeIndexToPreviousTempOne()
+{
+	Minus();
+	Branch();	// Ends if we're currently at a 1
+		Plus();	// Reverse what we did before our loop
+		Left();	// Decrease current index
+		Left();	// Decrease current index
+		Minus();
+	Loop();		// If current index is 1, ends
+
+	// Do we want to clear our 1 here?
 }
 
 void BrainfuckTool::Not()
@@ -360,8 +200,174 @@ void BrainfuckTool::Not()
 		Plus();		// Make our 0 into a 1
 		Right();
 		Minus();	// Reset our neighbor
-	Loop();
+		Loop();
 	Left();
+}
+
+
+/// Macro value functions (affects current data slot with immediate values)
+
+void BrainfuckTool::AddValue(int value)
+{
+	for (int i = 0; i < value; i++)
+	{
+		BrainfuckTool::Plus();
+	}
+}
+
+void BrainfuckTool::SubtractValue(int value)
+{
+	for (int i = 0; i < value; i++)
+	{
+		BrainfuckTool::Minus();
+	}
+}
+
+// This currently assumes that the value we are setting is 0
+void BrainfuckTool::SetValue(int value)
+{
+	AddValue(value);
+}
+
+void BrainfuckTool::SetArray(char inputArray[], int arrayLength, int arrayWidth)
+{
+	for (int i = 0; i < arrayLength; i++)
+	{
+		SetValue(inputArray[i]);
+		Right();
+		Right();
+
+		if (i % arrayWidth == arrayWidth - 1)
+		{
+			SetValue('\n');
+			Right();
+			Right();
+		}
+	}
+}
+
+
+/// Macro offset functions
+
+// Moves value from current memory position to offset from current memory position, assuming that the destination contains 0
+// Does this mean that this could be generally considered "AddTo"? for the cases that are non-zero? (But that doesn't imply the move...)
+// Not sure how useful this actually is, just happened to write it by accident when trying to make a more useful function
+// (Lightly tested)
+void BrainfuckTool::MoveToOffset(int offset)
+{
+	Branch();						// If current value is 0 we end up at associated loop; we'd be done in that case anyway so that's perfect
+		ChangeIndexRelative(offset);	// We move to our destination
+		Plus();							// We add 1 to our destination
+		ChangeIndexRelative(-offset);	// We move back to our source
+		Minus();						// We subtract 1 from our source
+	Loop();							// If our source is 0, stop looping
+}
+
+void BrainfuckTool::AddToOffset(int offset)
+{
+	int origininalOffset = 0;
+	int counter = 1;
+
+	NewTempVariable();
+	ChangeIndexRelative(origininalOffset - counter);
+	MoveToOffset(counter - origininalOffset);
+
+	ChangeIndexRelative(counter - origininalOffset);
+
+	Branch();						// If current value is 0 we end up at associated loop; we'd be done in that case anyway so that's perfect
+		ChangeIndexRelative(origininalOffset - counter);	// We change index to our origin (which now contains 0)
+		Plus();									// We add 1 to our origin
+		ChangeIndexRelative(offset);			// We change index to our destination
+		Plus();									// We add 1 our destination
+		ChangeIndexRelative(-offset);			// We go back to origin
+		ChangeIndexRelative(counter - origininalOffset);	// We change index to our counter
+		Minus();								// Subtract 1 from our counter
+		Loop();							// Loop till counter is 0
+	ChangeIndexRelative(origininalOffset - counter);
+}
+
+// I suspect there's a bug here, we hang on w currently
+void BrainfuckTool::SubtractFromOffset(int offset)
+{
+	int originalOffset = 0;
+	int counter = 1;
+
+	NewTempVariable();
+	ChangeIndexRelative(originalOffset - counter);
+	MoveToOffset(counter);
+
+	ChangeIndexRelative(counter - originalOffset);
+
+	Branch();						// If current value is 0 we end up at associated loop; we'd be done in that case anyway so that's perfect
+		ChangeIndexRelative(originalOffset - counter);	// We change index to our origin (which now contains 0)
+		Plus();							// We add 1 to our origin
+		ChangeIndexRelative(offset);	// We change index to our destination
+		Minus();						// We subtract 1 from our destination
+		ChangeIndexRelative(-offset);			// We go back to origin
+		ChangeIndexRelative(counter - originalOffset);	// We change index to our counter
+		Minus();						// Subtract 1 from our counter
+		Loop();							// Loop till counter is 0
+	ChangeIndexRelative(originalOffset - counter);
+}
+
+// Copies value from current memory position to offset from current memory positon, assuming that the destination contains 0
+// (Lightly tested)
+void BrainfuckTool::CopyToOffset(int offset)
+{
+	AddToOffset(offset);
+}
+
+
+/// Macro output functions
+
+void BrainfuckTool::OutString(int length)
+{
+	while (length > 0)
+	{
+		Out();
+		Right();
+		Right();
+		length--;
+	}
+}
+
+
+/// Macro variable tracking functions (returns index of data, these should be set before any other commands)
+
+// One of the "memory aware" functions, this will set the current variable to a value and move over 2 in prep for more variable declarations
+int BrainfuckTool::NewVariable(int value)
+{
+	int returnVal = virtualDataIndex;
+	SetValue(value);
+	Right();
+	Right();
+	return returnVal;
+}
+
+
+// We're only going to use this for our map, so it doesn't prep for more variables; gonna put the map at the end
+int BrainfuckTool::NewArray(char inputArray[], int arrayLength, int arrayWidth)
+{
+	int returnVal = virtualDataIndex;
+	SetArray(inputArray, arrayLength, arrayWidth);
+	return returnVal;
+}
+
+
+/// Macro data index modification functions
+
+void BrainfuckTool::ChangeIndexRelative(int offset)
+{
+	while(offset > 0)
+	{
+		Right();
+		offset--;
+	}
+	while(offset < 0)
+	{
+		Left();
+		offset++;
+	}
 }
 
 void BrainfuckTool::ChangeIndexRelativeToValueAtOffset(int offset)
@@ -415,50 +421,8 @@ void BrainfuckTool::ChangeIndexRelativeToValueAtOffset(int offset)
 	Left();
 }
 
-void BrainfuckTool::ChangeIndexLeftRelativeToValueAtOffset(int offset)
-{
-	int originalOffset = 0; // It's always going to be 0 but better to make it into a variable so it's named
-	int tempIndex = 1;
-	NewTempVariable();
 
-
-	ChangeIndexRelative(offset - tempIndex);
-	CopyToOffset(tempIndex - offset);
-	ChangeIndexRelative(tempIndex - offset);
-	Branch();
-		// Problem: At the end of this loop we need to decrese a variable from a known position, but we can't get back to our new position after that
-		// Idea: We tab over into the temp variables between here and the destination, adding 1 to them, and then moving right through all of them at the end
-		// To explain more: if each loop we go out by 2 till we reach a 0 in a temp variable, then we add 1 to that temp variable and loop again, till we get where we want
-		// Bonus problem: I can't "ChangeIndexAbsolute" from unknown/non-absolute coordinates, which is why I scan BACK to get to the origin instead of just "absoluting" there
-		ChangeIndexToPreviousTempZero();		// Brings us to the next 0 on the "temp track"
-		Plus();								// Add one on the temp track at this position, so it's not a zero next time (We'll have to clean this up later too...)
-		ChangeIndexToNextTempZero();		// Fly back to our last untarnished 0 which should be 1 right of origin
-		Left();
-		Left();								// Now at tempIndex
-		Minus();							// Finally we get to reduce our counter by 1!
-	Loop();
-
-	Plus();		// This is because I blew away tempIndex even though it's part of my breadcrumb trail
-
-	// We're already on the Temp track here so I removed these commands, hopefully everything will still work (gonna break so much stuff...)
-	////ChangeIndexAbsolute(origin);
-	//Left();		// From tempIndex to origin
-	//NewTempVariable();	// Temp track
-
-	// At this point we've got a pathway of ones we can follow to find where our @ symbol is (give or take 1? programmer's curse)
-	// Which is where we want to end on, so we should pick up our 1 debris as we go
-	Branch();
-		Minus();			// Changes 1 to 0 in most cases, till we get to the end where it changes 0 to 255 or something, who cares, we'll kill it in the next loop
-		Branch();
-			Minus();		// Clear the final 255 so we are clean and can drop out of this creative mess
-		Loop();
-		Left();
-		Left();
-	Loop();
-
-
-	Right();
-}
+/// Game logic that probably shouldn't be here
 
 void BrainfuckTool::PlayerLogic(int inputCharacterIndex, int wIndex, int aIndex, int sIndex, int dIndex, int wIndexTemp, int aIndexTemp, int sIndexTemp, int dIndexTemp, int playerPositionIndex, int playerPositionIndexTemp, int levelIndex, int widthIndex)
 {
@@ -574,4 +538,12 @@ void BrainfuckTool::PlayerLogic(int inputCharacterIndex, int wIndex, int aIndex,
 	SetZero();
 
 	ChangeIndexRelative(origin - playerPositionIndexTemp);
+}
+
+
+/// Private and practically useless function
+
+void BrainfuckTool::NewTempVariable()
+{
+	Right();
 }
--- BrainfuckInterpreter/BrainfuckTool.h
+++ BrainfuckInterpreter/BrainfuckTool.h
@@ -15,29 +15,38 @@ public:
 	void Branch(bool waitToResolve = true);
 	void Loop(bool resolve = true);
 
-	/// Macro functionality
-	// This currently assumes that the value we are setting is 0
-	void SetValue(int value);
-	void AddValue(int value);
-	void SubtractValue(int value);
-	void SetArray(char inputArray[], int arrayLength, int arrayWidth = 0);
-	int NewVariable(int value);
-	int NewArray(char inputArray[], int arrayLength, int arrayWidth);
-	void ChangeIndexRelative(int offset);
-	void ChangeIndexRelativeToValueAtOffset(int offset);
-	void ChangeIndexLeftRelativeToValueAtOffset(int offset);
-	void ChangeIndexToNextZero();
+	/// Pure brainfuck
+	void SetZero();
 	void ChangeIndexToNextTempZero();
 	void ChangeIndexToPreviousTempZero();
 	void ChangeIndexToPreviousTempOne();
-	void CopyToOffset(int offset);
-	void SubtractFromOffset(int offset);
-	void AddToOffset(int offset);
-	void SetZero();
 	void Not();
-	void PlayerLogic(int inputCharacterIndex, int wIndex, int aIndex, int sIndex, int dIndex, int wIndexTemp, int aIndexTemp, int sIndexTemp, int dIndexTemp, int playerPositionIndex, int playerPositionIndexTemp, int levelIndex, int widthIndex);
+
+	/// Macro value functions (affects current data slot with immediate values)
+	void AddValue(int value);
+	void SubtractValue(int value);
+	void SetValue(int value);
+	void SetArray(char inputArray[], int arrayLength, int arrayWidth = 0);
+
+	/// Macro offset functions (affects offset data slot with current data slot values)
 	void MoveToOffset(int offset);
+	void AddToOffset(int offset);
+	void SubtractFromOffset(int offset);
+	void CopyToOffset(int offset);
+
+	/// Macro output functions
 	void OutString(int length);
+
+	/// Macro variable tracking functions (returns index of data, these should be set before any other commands)
+	int NewVariable(int value);
+	int NewArray(char inputArray[], int arrayLength, int arrayWidth);
+
+	/// Macro data index modification functions
+	void ChangeIndexRelative(int offset);
+	void ChangeIndexRelativeToValueAtOffset(int offset);
+
+	/// Game logic that probably shouldn't be here
+	void PlayerLogic(int inputCharacterIndex, int wIndex, int aIndex, int sIndex, int dIndex, int wIndexTemp, int aIndexTemp, int sIndexTemp, int dIndexTemp, int playerPositionIndex, int playerPositionIndexTemp, int levelIndex, int widthIndex);
 
 	BFVM bfvm;
 
